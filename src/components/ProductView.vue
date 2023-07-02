ccb1f7449afc493303d7f7d00ffca316261e46b2
akpevwe11 <essiakpevwepraise@gmail.com>
2023-07-02 13:15:32 +0100

    vue ecommerce app

---
+++ src/components/ProductView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-2 m-auto">
+    <button
+      @click="this.$parent.$parent.$parent.$parent.$emit('addItem', item)"
+      class="btn btn-success"
+    >
+      +
+    </button>
+  </div>
+  <div class="col-sm-4">
+    <img class="img-fluid d-block" :src="item.image" :alt="item.name" />
+  </div>
+  <div class="col">
+    <h3 class="text-primary">{{ item.name }}</h3>
+    <p class="mb-0">{{ item.description }}</p>
+    <div class="h5 float-right">
+      <span class="label"></span><CurrPrice :amt="item.price"></CurrPrice>
+    </div>
+  </div>
+</template>
+
+<script>
+import CurrPrice from '@/components/CurrPrice'
+export default {
+  props: ['item'],
+  emits: ['addItem'],
+  components: {
+    CurrPrice
+  }
+}
+</script>
